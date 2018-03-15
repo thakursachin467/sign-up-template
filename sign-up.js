6148d105105d07ccfd73482ba3329dc1921c53f7
thakursachin467 <thakursachin467@gmail.com>
2018-03-15 19:46:23 +0530

    removed firebase info

--- sign-up.js
+++ sign-up.js
@@ -1,14 +1,18 @@
  var config = {
-    apiKey: "AIzaSyAlPs7A1rzshMzOVouOIZd9vn6Vtjdpw6Y",
-    authDomain: "questionnaire-java.firebaseapp.com",
-    databaseURL: "https://questionnaire-java.firebaseio.com",
-    projectId: "questionnaire-java",
-    storageBucket: "questionnaire-java.appspot.com",
-    messagingSenderId: "167839386903"
+    apiKey: "Your API-KEY",
+    authDomain: "YOUR PROJECT DOMIAN",
+    databaseURL: "YOUR DATABASE URL",
+    projectId: "YOUR PROJECT ID",
+    storageBucket: "STORAGE LINK",
+    messagingSenderId: "YOUR SENDER ID"
   };
 
+  //ALL THE ABOVE INFO CAN BE FOUND IN YOUR FIREBASE CONSOLE AT Add Firebase to your web app SECTION JUST COPY AND PASTE THE ETIRE SECTION AS IT IS
+  // FIREBASE LINK https://firebase.google.com
 
+  
   firebase.initializeApp(config);
+  
 
 
 
@@ -17,58 +21,87 @@ document.getElementById('button-1').addEventListener('click', submitform);
 
 //call this on submit
 function submitform(e) {
-	e.preventDefault();
+  e.preventDefault();
 //get values
-console.log('here');
+
 var name=formvalues('first_name');
 var email=formvalues('email');
 var password=formvalues('password');
 var password1=formvalues('password1');
 
 
-
-
-//save user only ifpasswords match
+if(name.length!=0 && email.length!=0 && password.length!=0) {
+//save user only if passwords match
 if(password==password1) {
 
-
-
+      
+      document.querySelector('.alert').style.display='block';
       var password=password;
       var email=email;
       firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-    alert(errorMessage);
+  
+  document.querySelector('.alert').innerHTML=errorMessage;
+  document.querySelector('.alert').style.display='block';
+  
+
+    
     // ...
-  });
+});
 
 
 
-
-
-//show this alert when user  is created
-document.querySelector('.alert').style.display='block';
 
 
 
 //hide alert after some time
 setTimeout(function(){
-	document.querySelector('.alert').style.display='none';
-	document.getElementById("registration").reset();
+  
+  document.querySelector('.alert').style.display='none';
+  document.querySelector('.alert').innerHTML='Account Created';
+  document.getElementById("registration").reset();
 
-},3000);
+
+},3000); 
+
+
+
+
+
 }
+
 
 //run this if passwords did not match
 else {
 
-		alert('your password did not match');
-		document.getElementById("registration").reset();
+  document.querySelector('.alert').innerHTML='Password did not match'
+  document.querySelector('.alert').style.display='block';
+  setTimeout(function(){
+    document.querySelector('.alert').style.display='none';
+  },3000);
+    
 
 
 
 }
+} 
+
+
+//this will run if all fields are empty
+ else {
+  document.querySelector('.alert').innerHTML='All Fields Are Mandatory'
+  document.querySelector('.alert').style.display='block';
+  setTimeout(function(){
+    document.querySelector('.alert').style.display='none';
+  },3000);
+
+
+}
+
+
 
 
 }
@@ -76,9 +109,10 @@ else {
 
 function formvalues(id) {
 
-console.log(document.getElementById(id).value);
+
 return document.getElementById(id).value;
 
 
 
 }
+
