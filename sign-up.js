ef15bdb3a04a5dd1cfa50176d4bb4e328eba879b
thakursachin467 <thakursachin467@gmail.com>
2018-03-21 02:47:47 +0530

    added annimations

--- sign-up.js
+++ sign-up.js
@@ -116,3 +116,72 @@ return document.getElementById(id).value;
 
 }
 
+
+  // get 2D context
+  var ctx = document.querySelector("canvas").getContext("2d"),
+  
+      // dash-length for off-range
+        dashLen = 220,
+    
+    // we'll update this, initialize
+        dashOffset = dashLen,
+    
+    // some arbitrary speed
+        speed = 15,
+    
+    // the text we will draw
+        txt = "Your Page Name",
+    
+    // start position for x and iterator
+    x = 40, i = 0;
+
+     // Comic Sans?? Let's make it useful for something ;) w/ fallbacks
+    ctx.font = "80px chalkfont"; 
+  
+  // to avoid spikes we can join each line with a round joint
+  ctx.lineJoin = "round";
+  
+  // some color, lets use a black pencil
+    ctx.strokeStyle = ctx.fillStyle = "#ffffff";
+
+
+
+    (function loop() {
+      // clear canvas for each frame
+      ctx.clearRect(x, 0, 60, 150);
+      
+      // calculate and set current line-dash for this char
+      ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]);
+      
+      // reduce length of off-dash
+      dashOffset -= speed;
+      
+      // draw char to canvas with current dash-length
+      ctx.strokeText(txt[i], x, 90);
+
+      // char done? no, the loop
+      if (dashOffset > 0) requestAnimationFrame(loop);
+      else {
+      
+        // ok, outline done, lets fill its interior before next
+        ctx.fillText(txt[i], x, 90);
+        
+        // reset line-dash length
+        dashOffset = dashLen;
+        
+        // get x position to next char by measuring what we have drawn
+        // notice we offset it a little by random to increase realism
+        x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
+        
+        // lets use an absolute transform to randomize y-position a little
+        ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random());
+        
+        // and just cause we can, rotate it a little too to make it even
+        // more realistic
+        ctx.rotate(Math.random() * 0.009);
+        
+        // if we still have chars left, loop animation again for this char
+        if (i < txt.length) requestAnimationFrame(loop);
+      }
+    })();  // just to self-invoke the loop
+
